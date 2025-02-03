653f8941d702b6e0597806f11f5ae3dce3574240
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2025-02-03 18:33:18 +0300

    cf domain

--- certbot/launch.sh
+++ certbot/launch.sh
@@ -8,3 +8,7 @@ docker compose run --rm misis-certbot renew --cert-name excursion.misis.space --
 
 #map
 docker compose run --rm misis-certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d "map.misis.space"
+
+#cf
+docker compose run --rm misis-certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d "cf.misis.space,cf-api.misis.space"
+docker compose run --rm misis-certbot renew --cert-name cf.misis.space --webroot-path /var/www/certbot/ --dry-run
--- nginx/nginx_prepare_run.sh
+++ nginx/nginx_prepare_run.sh
@@ -3,4 +3,5 @@ envsubst '${MISISSPACE_DOMAIN} ${ANY_ELSE_DOMAIN}' < /etc/nginx/nginx.conf.templ
 envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/misisspace_ssl.conf.template > /etc/nginx/misisspace_ssl.conf
 envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/excursion.misisspace_ssl.conf.template > /etc/nginx/excursion.misisspace_ssl.conf
 envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/map.misisspace_ssl.conf.template > /etc/nginx/map.misisspace_ssl.conf
+envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/cf.misisspace_ssl.conf.template > /etc/nginx/cf.misisspace_ssl.conf
 nginx -g "daemon off;"
