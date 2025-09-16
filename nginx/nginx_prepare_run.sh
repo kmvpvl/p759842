4dc7cddb2f113c9185b3423a9908dcea86c2a3cd
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2025-09-16 22:00:51 +0200

    df draft 5

--- nginx/nginx_prepare_run.sh
+++ nginx/nginx_prepare_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-envsubst '${MISISSPACE_DOMAIN} ${ANY_ELSE_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${MISISSPACE_DOMAIN} ${DOLCEFORTE_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/misisspace_ssl.conf.template > /etc/nginx/misisspace_ssl.conf
 envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/cf.misisspace_ssl.conf.template > /etc/nginx/cf.misisspace_ssl.conf
 
-envsubst '${DOLCEFORTE_DOMAIN} ${ANY_ELSE_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 envsubst '${DOLCEFORTE_DOMAIN}' < /etc/nginx/dolceforte_ssl.conf.template > /etc/nginx/dolceforte_ssl.conf
 nginx -g "daemon off;"
