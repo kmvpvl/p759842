c3bb0cce98f8ac7d9ed1d417a17b3e7c3a64d673
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2026-03-23 15:26:13 +0100

    shome

--- nginx/nginx_prepare_run.sh
+++ nginx/nginx_prepare_run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-envsubst '${MISISSPACE_DOMAIN} ${DOLCEFORTE_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/misisspace_ssl.conf.template > /etc/nginx/misisspace_ssl.conf
-envsubst '${MISISSPACE_DOMAIN}' < /etc/nginx/cf.misisspace_ssl.conf.template > /etc/nginx/cf.misisspace_ssl.conf
+envsubst '${MISISSPACE_DOMAIN} ${DOLCEFORTE_DOMAIN} ${PLUTCHART_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${MISISSPACE_DOMAIN} ${DOLCEFORTE_DOMAIN} ${PLUTCHART_DOMAIN}' < /etc/nginx/misisspace_ssl.conf.template > /etc/nginx/misisspace_ssl.conf
 
-envsubst '${DOLCEFORTE_DOMAIN}' < /etc/nginx/dolceforte_ssl.conf.template > /etc/nginx/dolceforte_ssl.conf
+envsubst '${MISISSPACE_DOMAIN} ${DOLCEFORTE_DOMAIN} ${PLUTCHART_DOMAIN}' < /etc/nginx/dolceforte_ssl.conf.template > /etc/nginx/dolceforte_ssl.conf
+
+envsubst '${MISISSPACE_DOMAIN} ${DOLCEFORTE_DOMAIN} ${PLUTCHART_DOMAIN}' < /etc/nginx/plutchart_ssl.conf.template > /etc/nginx/plutchart_ssl.conf
+envsubst '${MISISSPACE_DOMAIN} ${DOLCEFORTE_DOMAIN} ${PLUTCHART_DOMAIN}' < /etc/nginx/bckp_ssl.conf.template > /etc/nginx/bckp_ssl.conf
+
 nginx -g "daemon off;"
